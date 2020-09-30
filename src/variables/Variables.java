2702a87f586460765467ea83d27e0a8b140a67ea
luisviloria <67843161+luisviloria@users.noreply.github.com>
2020-09-30 18:29:35 -0300

    Initial commit

---
+++ src/variables/Variables.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        // Variables 	
+		int numero1;
+                int numero2;
+                int suma;
+                int edad;
+		String nombre;
+// Constante	
+		final int NUMERO_3 = 4;
+// Ingreso los valores a la variables numero1 y numero2
+		numero1 = 30;
+		numero2 = 40;
+// operación suma
+                suma = numero1+numero2;
+// Muestro en pantalla las dos variables y el resultado de la suma
+	      System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + suma);
+
+// Cambio los valores de numero1,numero2 y vuelvo a realizar la operación de suma
+		numero1=100;
+		numero2=50;
+//operación suma
+		suma = numero1+numero2;
+// Muestro en pantalla las dos variables y el resultado de la suma
+		 System.out.println("La suma es " + numero1 + " + " + numero2 + " = " + suma);
+
+// Instancio y muestro en pantalla la variable nombre y su edad
+		nombre = "Luis";
+                edad = 30;
+
+// Muestro en pantalla la constante
+                system.out.println("La Constante es:" + NUMERO3 = 4);
+
+    }
+    
+}
+
